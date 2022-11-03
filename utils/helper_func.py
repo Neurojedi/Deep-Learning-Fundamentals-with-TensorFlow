c875671bc6c01742e36b031d754db9fc6fbdbe00
Görkem Akgül <84977466+Neurojedi@users.noreply.github.com>
2022-11-03 23:36:50 +0300

    Add files via upload

---
+++ utils/helper_func.py
@@ -0,0 +1,518 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import sklearn
+import sklearn.datasets
+import sklearn.datasets
+import sklearn.linear_model
+import scipy.io
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0, x)
+
+    return s
+
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation (and computes the loss) presented in Figure 2.
+
+    Arguments:
+    X -- input dataset, of shape (input size, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                    W1 -- weight matrix of shape ()
+                    b1 -- bias vector of shape ()
+                    W2 -- weight matrix of shape ()
+                    b2 -- bias vector of shape ()
+                    W3 -- weight matrix of shape ()
+                    b3 -- bias vector of shape ()
+
+    Returns:
+    loss -- the loss function (vanilla logistic loss)
+    """
+
+    # retrieve parameters
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    z1 = np.dot(W1, X) + b1
+    a1 = relu(z1)
+    z2 = np.dot(W2, a1) + b2
+    a2 = relu(z2)
+    z3 = np.dot(W3, a2) + b3
+    a3 = sigmoid(z3)
+
+    cache = (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3)
+
+    return a3, cache
+
+
+def backward_propagation(X, Y, cache):
+    """
+    Implement the backward propagation presented in figure 2.
+
+    Arguments:
+    X -- input dataset, of shape (input size, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat)
+    cache -- cache output from forward_propagation()
+
+    Returns:
+    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+    """
+    m = X.shape[1]
+    (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3) = cache
+
+    dz3 = 1. / m * (a3 - Y)
+    dW3 = np.dot(dz3, a2.T)
+    db3 = np.sum(dz3, axis=1, keepdims=True)
+
+    da2 = np.dot(W3.T, dz3)
+    dz2 = np.multiply(da2, np.int64(a2 > 0))
+    dW2 = np.dot(dz2, a1.T)
+    db2 = np.sum(dz2, axis=1, keepdims=True)
+
+    da1 = np.dot(W2.T, dz2)
+    dz1 = np.multiply(da1, np.int64(a1 > 0))
+    dW1 = np.dot(dz1, X.T)
+    db1 = np.sum(dz1, axis=1, keepdims=True)
+
+    gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
+                 "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
+                 "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+
+def update_parameters(parameters, grads, learning_rate):
+    """
+    Update parameters using gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters
+    grads -- python dictionary containing your gradients, output of n_model_backward
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+                  parameters['W' + str(i)] = ...
+                  parameters['b' + str(i)] = ...
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for k in range(L):
+        parameters["W" + str(k + 1)] = parameters["W" + str(k + 1)] - learning_rate * grads["dW" + str(k + 1)]
+        parameters["b" + str(k + 1)] = parameters["b" + str(k + 1)] - learning_rate * grads["db" + str(k + 1)]
+
+    return parameters
+
+
+def compute_loss(a3, Y):
+    """
+    Implement the loss function
+
+    Arguments:
+    a3 -- post-activation, output of forward propagation
+    Y -- "true" labels vector, same shape as a3
+
+    Returns:
+    loss - value of the loss function
+    """
+
+    m = Y.shape[1]
+    logprobs = np.multiply(-np.log(a3), Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+    loss = 1. / m * np.nansum(logprobs)
+
+    return loss
+
+
+def load_cat_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
+    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    train_set_x_orig = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_orig = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+    train_set_x = train_set_x_orig / 255
+    test_set_x = test_set_x_orig / 255
+
+    return train_set_x, train_set_y, test_set_x, test_set_y, classes
+
+
+def predict(X, y, parameters):
+    """
+    This function is used to predict the results of a  n-layer neural network.
+
+    Arguments:
+    X -- data set of examples you would like to label
+    parameters -- parameters of the trained model
+
+    Returns:
+    p -- predictions for the given dataset X
+    """
+
+    m = X.shape[1]
+    p = np.zeros((1, m), dtype=np.int)
+
+    # Forward propagation
+    a3, caches = forward_propagation(X, parameters)
+
+    # convert probas to 0/1 predictions
+    for i in range(0, a3.shape[1]):
+        if a3[0, i] > 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+
+    # print results
+    print("Accuracy: " + str(np.mean((p[0, :] == y[0, :]))))
+
+    return p
+
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+def predict_dec(parameters, X):
+    """
+    Used for plotting decision boundary.
+
+    Arguments:
+    parameters -- python dictionary containing your parameters
+    X -- input data of size (m, K)
+
+    Returns
+    predictions -- vector of predictions of our model (red: 0 / blue: 1)
+    """
+
+    # Predict using forward propagation and a classification threshold of 0.5
+    a3, cache = forward_propagation(X, parameters)
+    predictions = (a3 > 0.5)
+    return predictions
+
+
+def load_dataset():
+    np.random.seed(1)
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+    np.random.seed(2)
+    test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
+    # Visualize the data
+    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    test_X = test_X.T
+    test_Y = test_Y.reshape((1, test_Y.shape[0]))
+    return train_X, train_Y, test_X, test_Y
+
+
+def gradient_check_n_test_case():
+    np.random.seed(1)
+    x = np.random.randn(4, 3)
+    y = np.array([1, 1, 0])
+    W1 = np.random.randn(5, 4)
+    b1 = np.random.randn(5, 1)
+    W2 = np.random.randn(3, 5)
+    b2 = np.random.randn(3, 1)
+    W3 = np.random.randn(1, 3)
+    b3 = np.random.randn(1, 1)
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+
+    return x, y, parameters
+
+
+def dictionary_to_vector(parameters):
+    """
+    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    """
+    keys = []
+    count = 0
+    for key in ["W1", "b1", "W2", "b2", "W3", "b3"]:
+
+        # flatten parameter
+        new_vector = np.reshape(parameters[key], (-1, 1))
+        keys = keys + [key] * new_vector.shape[0]
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta, keys
+
+
+def vector_to_dictionary(theta):
+    """
+    Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+    """
+    parameters = {}
+    parameters["W1"] = theta[:20].reshape((5, 4))
+    parameters["b1"] = theta[20:25].reshape((5, 1))
+    parameters["W2"] = theta[25:40].reshape((3, 5))
+    parameters["b2"] = theta[40:43].reshape((3, 1))
+    parameters["W3"] = theta[43:46].reshape((1, 3))
+    parameters["b3"] = theta[46:47].reshape((1, 1))
+
+    return parameters
+
+
+def gradients_to_vector(gradients):
+    """
+    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
+    """
+
+    count = 0
+    for key in ["dW1", "db1", "dW2", "db2", "dW3", "db3"]:
+        # flatten parameter
+        new_vector = np.reshape(gradients[key], (-1, 1))
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta
+
+
+def load_2D_dataset():
+    data = scipy.io.loadmat('datasets/data.mat')
+    train_X = data['X'].T
+    train_Y = data['y'].T
+    test_X = data['Xval'].T
+    test_Y = data['yval'].T
+
+    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
+
+    return train_X, train_Y, test_X, test_Y
+
+
+def initialize_parameters(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    b1 -- bias vector of shape (layer_dims[l], 1)
+                    Wl -- weight matrix of shape (layer_dims[l-1], layer_dims[l])
+                    bl -- bias vector of shape (1, layer_dims[l])
+
+    Tips:
+    - For example: the layer_dims for the "Planar Data classification model" would have been [2,2,1].
+    This means W1's shape was (2,2), b1 was (1,2), W2 was (2,1) and b2 was (1,1). Now you have to generalize it!
+    - In the for loop, use parameters['W' + str(l)] to access Wl, where l is the iterative integer.
+    """
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) / np.sqrt(layer_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+
+def compute_cost(a3, Y):
+    """
+    Implement the cost function
+
+    Arguments:
+    a3 -- post-activation, output of forward propagation
+    Y -- "true" labels vector, same shape as a3
+
+    Returns:
+    cost - value of the cost function
+    """
+    m = Y.shape[1]
+
+    logprobs = np.multiply(-np.log(a3), Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+    cost = 1. / m * np.nansum(logprobs)
+
+    return cost
+
+
+import numpy as np
+
+
+def compute_cost_with_regularization_test_case():
+    np.random.seed(1)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    W1 = np.random.randn(2, 3)
+    b1 = np.random.randn(2, 1)
+    W2 = np.random.randn(3, 2)
+    b2 = np.random.randn(3, 1)
+    W3 = np.random.randn(1, 3)
+    b3 = np.random.randn(1, 1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3}
+    a3 = np.array([[0.40682402, 0.01629284, 0.16722898, 0.10118111, 0.40682402]])
+    return a3, Y_assess, parameters
+
+
+def backward_propagation_with_regularization_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    cache = (np.array([[-1.52855314, 3.32524635, 2.13994541, 2.60700654, -0.75942115],
+                       [-1.98043538, 4.1600994, 0.79051021, 1.46493512, -0.45506242]]),
+             np.array([[0., 3.32524635, 2.13994541, 2.60700654, 0.],
+                       [0., 4.1600994, 0.79051021, 1.46493512, 0.]]),
+             np.array([[-1.09989127, -0.17242821, -0.87785842],
+                       [0.04221375, 0.58281521, -1.10061918]]),
+             np.array([[1.14472371],
+                       [0.90159072]]),
+             np.array([[0.53035547, 5.94892323, 2.31780174, 3.16005701, 0.53035547],
+                       [-0.69166075, -3.47645987, -2.25194702, -2.65416996, -0.69166075],
+                       [-0.39675353, -4.62285846, -2.61101729, -3.22874921, -0.39675353]]),
+             np.array([[0.53035547, 5.94892323, 2.31780174, 3.16005701, 0.53035547],
+                       [0., 0., 0., 0., 0.],
+                       [0., 0., 0., 0., 0.]]),
+             np.array([[0.50249434, 0.90085595],
+                       [-0.68372786, -0.12289023],
+                       [-0.93576943, -0.26788808]]),
+             np.array([[0.53035547],
+                       [-0.69166075],
+                       [-0.39675353]]),
+             np.array([[-0.3771104, -4.10060224, -1.60539468, -2.18416951, -0.3771104]]),
+             np.array([[0.40682402, 0.01629284, 0.16722898, 0.10118111, 0.40682402]]),
+             np.array([[-0.6871727, -0.84520564, -0.67124613]]),
+             np.array([[-0.0126646]]))
+    return X_assess, Y_assess, cache
+
+
+def forward_propagation_with_dropout_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    W1 = np.random.randn(2, 3)
+    b1 = np.random.randn(2, 1)
+    W2 = np.random.randn(3, 2)
+    b2 = np.random.randn(3, 1)
+    W3 = np.random.randn(1, 3)
+    b3 = np.random.randn(1, 1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3}
+
+    return X_assess, parameters
+
+
+def backward_propagation_with_dropout_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    cache = (np.array([[-1.52855314, 3.32524635, 2.13994541, 2.60700654, -0.75942115],
+                       [-1.98043538, 4.1600994, 0.79051021, 1.46493512, -0.45506242]]),
+             np.array([[True, False, True, True, True],
+                       [True, True, True, True, False]], dtype=bool), np.array([[0., 0., 4.27989081, 5.21401307, 0.],
+                                                                                [0., 8.32019881, 1.58102041, 2.92987024,
+                                                                                 0.]]),
+             np.array([[-1.09989127, -0.17242821, -0.87785842],
+                       [0.04221375, 0.58281521, -1.10061918]]), np.array([[1.14472371],
+                                                                          [0.90159072]]),
+             np.array([[0.53035547, 8.02565606, 4.10524802, 5.78975856, 0.53035547],
+                       [-0.69166075, -1.71413186, -3.81223329, -4.61667916, -0.69166075],
+                       [-0.39675353, -2.62563561, -4.82528105, -6.0607449, -0.39675353]]),
+             np.array([[True, False, True, False, True],
+                       [False, True, False, True, True],
+                       [False, False, True, False, False]], dtype=bool),
+             np.array([[1.06071093, 0., 8.21049603, 0., 1.06071093],
+                       [0., 0., 0., 0., 0.],
+                       [0., 0., 0., 0., 0.]]), np.array([[0.50249434, 0.90085595],
+                                                         [-0.68372786, -0.12289023],
+                                                         [-0.93576943, -0.26788808]]), np.array([[0.53035547],
+                                                                                                 [-0.69166075],
+                                                                                                 [-0.39675353]]),
+             np.array([[-0.7415562, -0.0126646, -5.65469333, -0.0126646, -0.7415562]]),
+             np.array([[0.32266394, 0.49683389, 0.00348883, 0.49683389, 0.32266394]]),
+             np.array([[-0.6871727, -0.84520564, -0.67124613]]), np.array([[-0.0126646]]))
+
+    return X_assess, Y_assess, cache
+
+
+def load_params_and_grads(seed=1):
+    np.random.seed(seed)
+    W1 = np.random.randn(2, 3)
+    b1 = np.random.randn(2, 1)
+    W2 = np.random.randn(3, 3)
+    b2 = np.random.randn(3, 1)
+
+    dW1 = np.random.randn(2, 3)
+    db1 = np.random.randn(2, 1)
+    dW2 = np.random.randn(3, 3)
+    db2 = np.random.randn(3, 1)
+
+    return W1, b1, W2, b2, dW1, db1, dW2, db2
+
+
+def load_dataset_opt():
+    np.random.seed(3)
+    train_X, train_Y = sklearn.datasets.make_moons(n_samples=300, noise=.2)  # 300 #0.2
+    # Visualize the data
+    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+
+    return train_X, train_Y
